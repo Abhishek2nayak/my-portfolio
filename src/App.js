6dfa6db904bef267648927852f1f2c84f0bd82e1
abhishek nayak <abhishek5nayak22@gmail.com>
2023-05-08 19:08:19 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar/Navbar";
+import './main.css'
+
+function App() {
+  return (
+    <>
+    <main className="main">
+      
+    
+      </main>
+      <Navbar/>
+    </>
+    
+  );
+}
+
+export default App;
