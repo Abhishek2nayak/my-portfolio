2f2a4f58196ecf9ba05e1a3f3ef06671f2e63bb6
Abhishek Nayak <94285920+Abhishek2nayak@users.noreply.github.com>
2023-05-15 17:31:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar/Navbar";
 import './main.css'
-import { BrowserRouter,Route,Link , Router, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Home from './components/Home/Home'
 import About from './components/About/About'
 import Skills from './components/Skills/Skills'
